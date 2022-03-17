ea81f541b3290fc5f5dbc7f028e5faae1fa7aa9e
D0rkKnight <shouhanzen@gmail.com>
2022-03-16 20:21:48 -0700

    GIL error

--- tetrisCore.c
+++ tetrisCore.c
@@ -47,7 +47,7 @@ static void installPiece(int p, int r, int *data) {
     pieceData[p][r] = mem;
 
     // A small change
-    // printf("A small change\n");
+    printf("A small change\n");
 
     return;
 }
